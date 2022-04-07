69ecc8b7f67d807cbbeaf481f05c953d52724712
blucky8649 <blucky4511@gmail.com>
2022-04-07 21:57:55 +0900

    FEAT: Paging 3, 리사이클러뷰 셋업

--- app/src/main/java/com/example/mymovieapp/util/Constants.kt
+++ app/src/main/java/com/example/mymovieapp/util/Constants.kt
@@ -4,8 +4,6 @@ object Constants {
     const val BASE_URL = "https://openapi.naver.com/"
     const val NAVER_ID = "AYKkGeV2L7WTKPN6ew27"
     const val NAVER_SECRET = "FSR3eh70zW"
-    const val START_PAGE = 1
     const val PAGE_SIZE = 10
     const val DELEY_500L = 500L
-
 }
